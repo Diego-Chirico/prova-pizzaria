2dcc33993256b45f8d0e5165c57284f1ddd2a953
Lyllys <lyllysgalhardo@gmail.com>
2021-04-08 11:00:44 -0300

    Botão de escolhas

--- avaliacaoFinal/js/script.js
+++ avaliacaoFinal/js/script.js
@@ -1,9 +1,138 @@
+const tamanhoPizza = [
+    {
+        tipo: "pequena",
+        tamanho: 20,
+        valor: 25.00
+    },
+    {
+        tipo: "media",
+        tamanho: 25,
+        valor: 30.00
+    },
+    {
+        tipo: "gigante",
+        tamanho: 35,
+        valor: 40.00
+    },
+    {
+        tipo: "familia",
+        tamanho: 40,
+        valor: 45.00
+    }
+]
+
+const tipoDeBorda = [
+    {
+        borda: "gorgonzola",
+        valor: 10.00
+    },
+    {
+        borda: "provolone",
+        valor: 8.00
+    },
+    {
+        borda: "catupiry",
+        valor: 5.00
+    },
+
+    {
+        borda: "cheddar",
+        valor: 5.00
+
+    },
+
+    {
+        borda: "sem borda",
+        valor: 0.00
+    }
+]
+const sabores = [
+    {
+        sabor: "Calabresa",
+        ingredientes: "mussarela,cebola e calabresa"
+    },
+    {
+        sabor: "4 queijos",
+        ingredientes: "mussarela,cheddar, parmesão, prato"
+    },
+    {
+        sabor: "Frango com catupiry",
+        ingredientes: "mussarela, frango e catupiry"
+    },
+    {
+        sabor: "Portuguesa",
+        ingredientes: "mussarela, pimentão, cebola, presunto, ovo"
+    },
+
+    {
+        sabor: "Camarão",
+        ingredientes: "mussarela, camarão, salsinha e cebolinha"
+    },
+    {
+        sabor: "Carne Seca",
+        ingredientes: "mussarela, carne seca,  cream cheese"
+    },
+    {
+        sabor: " Lombo Canadense",
+        ingredientes: " mussarela, lombo canadense, tomate, orégano, cebola"
+    },
+    {
+        sabor: "X - Fritas",
+        ingredientes: "mussarela, contra-filé, presunto, bacon, ovo, tomate, cebola, batata frita "
+    }
+]
+const bebidas = [
+    {
+        bebida: "Coca-Cola",
+        valor: 12.00
+    },
+    {
+        tipo: "Fanta",
+        valor: 10.00
+    },
+    {
+        tipo: "Guaraná Antartica",
+        valor: 10.00
+    },
+    {
+        tipo: "H2O",
+        valor: 8.00
+    },
+    {
+        tipo: "Del Valle",
+        valor: 7.00
+    }
+]
+const tipoDeEntrega = [
+    {
+        entrega: "Retirada no Balcão",
+        valor: 0.00
+    },
+    {
+        entrega: "Delivery",
+        valor: 5.00
+    }   
+]
+
+let dadosDoUsuario = []
 
 
-function meuPedido(){
 
- 
-    
+
+
+
+
+
+function meuPedido() {
+
     console.log("oi")
-    
+
+}
+
+function meuPedido() {
+
+
+
+    console.log("oi")
+
 }
