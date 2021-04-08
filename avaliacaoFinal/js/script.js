3fdacd91f348c5944af4a71444c33e7884bf2576
DanieldSantosFonseca <danielsfonseca@outlook.com>
2021-04-08 10:28:00 -0300

    tamanhos + css básico

--- avaliacaoFinal/js/script.js
+++ avaliacaoFinal/js/script.js
@@ -1,7 +1,28 @@
+const tamanhoPizza = [
+    {
+        tipo: "pequena",
+        tamanho: 20,
+        valor: 25.00
+    },
+    {
+        tipo: "media",
+        tamanho: 25,
+        valor: 30.00
+    },
+    {
+        tipo: "gigante",
+        tamanho: 35,
+        valor: 40.00
+    },
+    {
+        tipo: "familia",
+        tamanho: 40,
+        valor: 45.00
+    }
+]
 
-
-function meuPedido(){
+function meuPedido() {
 
     console.log("oi")
-    
+
 }
